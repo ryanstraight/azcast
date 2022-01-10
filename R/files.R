6ed1c729cdb68073c2c8e6787c156dd0409a0846
ryanstraight <ryanstraight@arizona.edu>
2022-01-10 14:30:18 -0700

    Getting rid of other functions

--- R/files.R
+++ R/files.R
@@ -24,11 +24,3 @@ use_file <- function(file, new_path) {
   file_copy(path, new_path)
 }
 
-#' Open .releaseignore file
-#'
-#' @export
-edit_release_ignore <- function() {
-  invisible(usethis::edit_file(".releaseignore"))
-}
-
-
